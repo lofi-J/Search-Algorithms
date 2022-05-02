9fb021b97782ba0ef66714beb6f84ef68419ce4a
lofi-J <jsj2505@gmail.com>
2022-05-02 23:33:42 +0900

    Add Linear Search

---
+++ LinearSearch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class LinearSearch
+{
+    public int LinearSearchFunc(int[] list, int target)
+    {
+        for (int i=0; i<list.Length; i++)
+        {
+            if (list[i] == target)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
